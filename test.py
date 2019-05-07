a2198080ea5aa97da96055444c8737c93071d929
ice1x <ice1x@gmail.com>
2019-05-07 23:36:59 +0500

    Add unittest. Add default args. Add Singleton

---
+++ test.py
@@ -0,0 +1,66 @@
+import unittest
+
+from trie import Trie
+
+
+IVAN_31_TT = {
+        'name': 'иван',
+        'age': 31,
+        'gender': True,
+        'type': True
+    }
+IRINA_23_FT = {
+        'name': 'ирина',
+        'age': 23,
+        'gender': False,
+        'type': True
+    }
+IO_3_TT = {
+        'name': 'ио',
+        'age': 3,
+        'gender': True,
+        'type': True
+    }
+IVANOVICH_51_TN = {
+        'name': 'иванович',
+        'age': 51,
+        'gender': True,
+        'type': None
+    }
+TEST_DATA_SMALL = [
+    IVAN_31_TT,
+    IRINA_23_FT,
+    IO_3_TT,
+    IVANOVICH_51_TN
+]
+
+
+class TestSmallTrieMethods(unittest.TestCase):
+    def setUp(self):
+        self.trie = Trie()
+        for person in TEST_DATA_SMALL:
+            self.trie.insert(
+                person['name'],
+                {
+                    'name': person['name'],
+                    'age': person['age'],
+                    'gender': person['gender'],
+                    'type': person['type']
+                }
+            )
+
+    def test_no_output_increment(self):
+        """
+        Regression
+        """
+        res1 = self.trie.get_by_prefix('иван')[:]
+        res2 = self.trie.get_by_prefix('иван')[:]
+        self.assertEqual(res1, res2)
+        # res = self.trie.get_by_prefix_sort_by('ив', 'age')
+        # print(len(res))
+        # print(self.trie.get_by_prefix_and_query("и", {"type": True, "gender": False}))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
--- trie.py
+++ trie.py
@@ -1,8 +1,6 @@
 """
 SQL-like query with prefix-tree:
 """
-from datetime import datetime
-
 
 GENDER = {
     True: 'Male',
@@ -18,6 +16,15 @@ TYPE_REVERSED = {v: k for k, v in TYPE.items()}
 GENDER_REVERSED = {v: k for k, v in GENDER.items()}
 
 
+class Singleton(type):
+    _instances = {}
+    
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
 class Node(object):
     """
     Class describes the data structure of node
@@ -30,10 +37,23 @@ class Node(object):
         self._SIZE = self.char.__sizeof__() + self.data.__sizeof__() + self.children.__sizeof__()
 
 
-class Trie(Node):
+class Trie(Node, metaclass=Singleton):
     """
     Class describes the tree hierarchy and routines to set/get data
     """
+
+    def __init__(
+            self,
+            char='%%',
+            data=[]
+    ):
+        self.char = char
+        self.data = data
+        super().__init__(
+            self.char,
+            self.data
+        )
+
     def insert(self, word, data):
         """
         Inserts word with attached data in the new or existent node
@@ -62,7 +82,7 @@ class Trie(Node):
         self._SIZE += data.__sizeof__()
         node.data.append(data)
 
-    def _find_last_node_by_prefix(self, prefix):
+    def _get_last_node_by_prefix(self, prefix):
         """
         Args:
             prefix(str): - word/name prefix to search node where last symbol of word/name appears
@@ -89,6 +109,14 @@ class Trie(Node):
             result(list): - contain list of data(dict) inside
         """
         def _get_data_by_child(parent, result):
+            """
+            Iterator
+            Args:
+                parent:
+                result:
+            Returns:
+                 (node.data)
+            """
             for key, value in parent.children.items():
                 result += value.data
                 result = _get_data_by_child(parent.children[key], result)
@@ -118,7 +146,7 @@ class Trie(Node):
         Return:
             (list): list of dict's
         """
-        return self._get_data_by_node(self._find_last_node_by_prefix(prefix))
+        return self._get_data_by_node(self._get_last_node_by_prefix(prefix))
 
     def get_by_prefix_sort_by(self, prefix, key_):
         """
@@ -129,7 +157,7 @@ class Trie(Node):
         Return:
             result(list): list of dict's
         """
-        return self._sort(self._get_data_by_node(self._find_last_node_by_prefix(prefix)), key_)
+        return self._sort(self._get_data_by_node(self._get_last_node_by_prefix(prefix)), key_)
 
     def get_by_prefix_and_query(self, prefix, query):
         """
@@ -161,7 +189,7 @@ class Trie(Node):
         Return:
             result(dict):
         """
-        tmp_result = self._find_last_node_by_prefix(word).data
+        tmp_result = self._get_last_node_by_prefix(word).data
         tmp_query = [(k, v) for k, v in query.items()]
         for i in tmp_result:
             for j in tmp_query:
@@ -172,54 +200,3 @@ class Trie(Node):
 
     def __len__(self):
         return self._SIZE
-
-
-if __name__ == "__main__":
-    trie = Trie('%%', [])
-
-    for person in [
-            {
-                'name': 'иван',
-                'age': 31,
-                'gender': True,
-                'type': True
-            },
-            {
-                'name': 'ирина',
-                'age': 23,
-                'gender': False,
-                'type': True
-            },
-            {
-                'name': 'ио',
-                'age': 3,
-                'gender': True,
-                'type': True
-            },
-            {
-                'name': 'иванович',
-                'age': 51,
-                'gender': True,
-                'type': None
-            }
-
-        ]:
-            trie.insert(
-                person['name'],
-                {
-                    'name': person['name'],
-                    'age': person['age'],
-                    'gender': person['gender'],
-                    'type': person['type']}
-            )
-
-    s = datetime.now()
-    res = trie.get_by_prefix('иван')
-    res = trie.get_by_prefix_sort_by('ив', 'age')
-    time_ = datetime.now() - s
-    for i in res:
-        print(i)
-    print(len(res))
-    print(time_)
-    print(trie.get_by_prefix_and_query("и", {"type": True, "gender": False}))
-    print(len(trie))
